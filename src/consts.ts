3103e1caa7509d05dd9e524de92f0ed050eb310c
Florian Kimmel <florianmkimmel@gmail.com>
2023-10-18 18:42:27 +0200

    Init project #2

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Laufend Entecken Podcast';
+export const SITE_DESCRIPTION = 'Der österreichische Laufpodcast';
+
+export const WORDPRESS_API_URL = "https://laufendentdecken-podcast.at/wp-json/wp/v2"
+export const WORDPRESS_API_EPISODES_URL = `${WORDPRESS_API_URL}/episodes`
