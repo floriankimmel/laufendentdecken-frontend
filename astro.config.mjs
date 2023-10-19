5ba46f52ae6ea24f4c2f3ebee171c01dc4a9eed5
Florian Kimmel <florianmkimmel@gmail.com>
2023-10-19 10:36:40 +0200

    Make opening a page faster

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import netlify from "@astrojs/netlify/functions";
+
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), prefetch()],
   output: "server",
   adapter: netlify()
 });
