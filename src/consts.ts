42b7fe9728fdb3c56c25dead73fb4d451a4a02aa
Florian Kimmel <florianmkimmel@gmail.com>
2023-10-20 18:38:31 +0200

    Load the entire episodes statically

--- src/consts.ts
+++ src/consts.ts
@@ -6,3 +6,5 @@ export const SITE_DESCRIPTION = 'Der österreichische Laufpodcast';
 
 export const WORDPRESS_API_URL = "https://laufendentdecken-podcast.at/wp-json/wp/v2"
 export const WORDPRESS_API_EPISODES_URL = `${WORDPRESS_API_URL}/episodes`
+
+export const PER_PAGE = 12
