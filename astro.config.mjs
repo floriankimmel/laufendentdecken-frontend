bc83f507fc49b70fe0aa44e8db869d4729d4b4be
Florian Kimmel <florianmkimmel@gmail.com>
2024-04-14 06:57:26 +0200

    Init new layout with astrowind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,94 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+import { defineConfig, squooshImageService } from 'astro/config';
+
 import sitemap from '@astrojs/sitemap';
-import netlify from "@astrojs/netlify/functions";
+import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
+import partytown from '@astrojs/partytown';
+import icon from 'astro-icon';
+import compress from '@playform/compress';
 
-import prefetch from "@astrojs/prefetch";
+import astrowind from './vendor/integration';
 
-// https://astro.build/config
+import { readingTimeRemarkPlugin, responsiveTablesRehypePlugin, lazyImagesRehypePlugin } from './src/utils/frontmatter.mjs';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const hasExternalScripts = false;
+const whenExternalScripts = (items = []) =>
+  hasExternalScripts
+    ? Array.isArray(items)
+      ? items.map((item) => item())
+      : [items()]
+    : [];
+
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), prefetch()],
-    output: "hybrid",
-  adapter: netlify(),
-    image: {
-        domains: ["laufendentdecken-podcast.at"],
-    }
+  output: 'static',
+
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    sitemap(),
+    mdx(),
+    icon({
+      include: {
+        tabler: ['*'],
+        'flat-color-icons': [
+          'template',
+          'gallery',
+          'approval',
+          'document',
+          'advertising',
+          'currency-exchange',
+          'voice-presentation',
+          'business-contact',
+          'database',
+        ],
+      },
+    }),
+
+    ...whenExternalScripts(() =>
+      partytown({
+        config: { forward: ['dataLayer.push'] },
+      })
+    ),
+
+    compress({
+      CSS: true,
+      HTML: {
+        'html-minifier-terser': {
+          removeAttributeQuotes: false,
+        },
+      },
+      Image: false,
+      JavaScript: true,
+      SVG: false,
+      Logger: 1,
+    }),
+
+    astrowind({
+      config: "./src/config.yaml"
+    }),
+  ],
+
+  image: {
+    service: squooshImageService(),
+    domains: ["cdn.pixabay.com"],
+  },
+
+  markdown: {
+    remarkPlugins: [readingTimeRemarkPlugin],
+    rehypePlugins: [responsiveTablesRehypePlugin, lazyImagesRehypePlugin],
+  },
+
+  vite: {
+    resolve: {
+      alias: {
+        '~': path.resolve(__dirname, './src'),
+      },
+    },
+  },
 });
